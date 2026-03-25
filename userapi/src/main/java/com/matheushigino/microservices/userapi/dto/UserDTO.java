3a6cb8021aee173e77c0b93d62693fd8058a226d
Matheus Higino <matheus.higino@yahoo.com.br>
2026-03-24 21:47:52 -0300

    config: Spring Data nos serviços e criacao da camada de dados (repository)

--- userapi/src/main/java/com/matheushigino/microservices/userapi/dto/UserDTO.java
+++ userapi/src/main/java/com/matheushigino/microservices/userapi/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.matheushigino.microservices.userapi.dto;
 
+import com.matheushigino.microservices.userapi.model.User;
 import jakarta.validation.constraints.NotBlank;
 
 import lombok.Getter;
@@ -23,4 +24,15 @@ public class UserDTO {
     private String email;
     private String telefone;
     private LocalDateTime dataCadastro;
+
+    public static UserDTO convert(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome(user.getNome());
+        userDTO.setEndereco(user.getEndereco());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setTelefone(user.getTelefone());
+        userDTO.setDataCadastro(user.getDataCadastro());
+        return userDTO;
+    }
 }
---
+++ userapi/src/main/java/com/matheushigino/microservices/userapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.matheushigino.microservices.userapi.repository;
+
+import com.matheushigino.microservices.userapi.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByCpf(String cpf);
+    List<User> queryByNomeLike(String nome);
+}
